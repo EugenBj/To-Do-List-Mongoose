dbbfe2ec74797e1d4110923712fe899a239ce636
Eugen Bejenaru <48156218+EugenBj@users.noreply.github.com>
2020-06-12 15:48:29 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -121,12 +121,6 @@ app.post("/delete", function(req, res) {
   }
 });
 
-// let port = process.env.PORT;
-// if (port == null || port = "") {
-//   port = 3000;
-// }
-// app.listen(port);
-
 app.listen(process.env.PORT || 3000, function(req, res) {
   console.log("Server successfully started.");
 });
